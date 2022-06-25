4ed12191a6fcd3153df59e2940f3a6c024fb429c
Mariangelica Rodriguez <gromarant@gmail.com>
2022-06-25 03:02:39 +0200

    Variables (const and let) lesson

---
+++ src/Bases/01-const-let.js
@@ -0,0 +1,9 @@
+//let and const
+    //'var' should not be used any more
+    const name = 'Mariangelica';
+    const lastName = 'Rodríguez';
+
+    let gettingValue = 5;
+
+    console.log ( name, lastName, gettingValue);
+
