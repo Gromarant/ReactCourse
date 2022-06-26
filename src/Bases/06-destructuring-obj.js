3047a3c84a969ba1d5892d69f23c67f0f5f5cc7c
Mariangelica Rodriguez <gromarant@gmail.com>
2022-06-26 02:34:04 +0200

    Add 06-destructuring-obj.js file

---
+++ src/Bases/06-destructuring-obj.js
@@ -0,0 +1,31 @@
+// destructuring assignment
+    //Object
+    const person = {
+        name: 'Tony',
+        age: 45,
+        password: 'Ironman',
+    }
+
+    const { name, age, password } = person; //Object destructuring
+
+    console.log ( name );
+    console.log ( age );
+    console.log ( password );
+
+ //set default value "range = 'Capitán"
+    const useContex = ({ name, age, range = 'Capitán', password }) => {
+        return {
+            passwordName: password,
+            howOld: age,
+            latlng: {
+                lat: 175.2855,
+                lng: -79.689,
+            },
+        }
+    }
+
+ //Straction of nested objects 'latlng: { lat, lng }'
+    const { passwordName, howOld, latlng: { lat, lng } } = useContex( person );
+
+    console.log( passwordName, howOld );
+    console.log(  lat, lng ); // 'latlng' is not exist anymore, it change in two variables 'lat' and 'lng'
