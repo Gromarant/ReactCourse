08fbc73d212a8415405a8a0d699ececd740dafc1
Mariangelica Rodriguez <gromarant@gmail.com>
2022-07-01 09:47:05 +0200

    Make HelloWordApp

---
+++ 03-counter-app-vite/src/HelloWordApp.jsx
@@ -0,0 +1,11 @@
+//  export function App() {
+//     return <h1> Hola mundo =) !! </h1>;
+// }
+
+//Function with shortCut 'rafc'    
+    export const HelloWordApp = () => {
+      return (
+        <h1>Hello Word App</h1>
+      )
+    }
+    
