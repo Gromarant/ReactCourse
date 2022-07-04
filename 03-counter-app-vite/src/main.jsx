08ddb541fd39c86cba2bb78ed3b04cca88095bec
Mariangelica Rodriguez <gromarant@gmail.com>
2022-07-04 15:56:08 +0200

    Add global styles to react, 39 lesson

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { FirstApp } from './FirstApp';
 // import { HelloWordApp } from './HelloWordApp';
+import './styles.css';
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
