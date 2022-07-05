9f5e672917b986140ad9544466166836f3c7b2ba
Mariangelica Rodriguez <gromarant@gmail.com>
2022-07-05 10:34:54 +0200

    Add lessons-40-to-42.jsx file

---
+++ 03-counter-app-vite/src/lessons-40-to-42.jsx
@@ -0,0 +1,88 @@
+//lesson 40 - Components comunication
+
+  //props access
+  const nameFunction = ( props ) => {       
+    <>
+      <h1>{ props.title }</h1>
+    </>
+  }
+
+//props access with destructuring
+  const TitleFunction = ( { title } ) => {
+    <>
+      <h1>{ title }</h1>
+    </>
+  }
+
+  export const FirstApp = ( { title, subtitle } ) => {
+    return (
+      <>
+          <h1>{ title }</h1> 
+          <p>{ subtitle + 25000 }</p>
+      </> 
+    )
+  }
+
+    // default value of props 
+      // export const FirstApp = ( { title = 'Hola mundo' } ) => {}
+
+//lesson 41 - propTypes
+  //this way the code generate an Error inside the component, it is not a good practice
+export const PropsAppErr = ( { title } ) => {
+    if( !title ) {
+      throw new Error( 'The title is missing' );
+    }
+    return (
+      <h1>{ title }</h1>
+    )
+  }
+
+  //Imported and used PropTypes, with Vite the PropTypes are not downlouded
+    import PropTypes from 'prop-types';
+    
+    export const PropsApp = ( { title } ) => {
+      return (
+        <h1>{ title }</h1>
+      )
+    }
+  
+    PropsApp.protoTypes = {
+      title: PropTypes.string.isRequired,
+    }
+
+//lesson 42 - DefaultProps 
+//DefaultProps in the top of the component
+  // export const DefPropsApp = ({
+  //   title,
+  //   subtitle = 'Gromarant',
+  // }) => {
+  //     return (
+  //      <>
+  //         <h1>{ title }</h1>
+  //         <h2>{ subtitle }</h2>
+  //       </>
+  //     )
+  //   }
+
+
+//DefaultProps in the bottom of the component
+  export const DefPropsApp = ({ title, subtitle, name }) => {
+      return (
+       <>
+          <h1>{ title }</h1>
+          <h2>{ subtitle }</h2>
+          <h3>{ name }</h3>
+        </>
+      )
+    }
+
+  DefPropsApp.defaultProps = {
+    name: 'Mariangelica Rodriguez',
+    subtitle: 'There is not subtitle',
+    title: 'There is not title',
+  }
+
+  DefPropsApp.protoTypes = {
+    subtitle: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  } 
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { DefPropsApp } from './FirstApp';
-// import { PropsApp } from './FirstApp';
-// import { FirstApp } from './FirstApp';
-// import { HelloWordApp } from './HelloWordApp';
 import './styles.css';
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        < DefPropsApp title="The salary of 50.000 Euros is for you" />
     </React.StrictMode>
 );
+
+
+// imports
+    // import { DefPropsApp } from './FirstApp';
+    // import { PropsApp } from './FirstApp';
+    // import { FirstApp } from './FirstApp';
+    // import { HelloWordApp } from './HelloWordApp';
 
 //Rendering of components
     // <React.StrictMode>
     //     {/* <HelloWordApp /> */}
     //     {/* <FirstApp subtitle={25000} /> */}
     //     < PropsApp title="The salary of 50.000 Euros is for you" />
+        // < DefPropsApp title="The salary of 50.000 Euros is for you" />
     // </React.StrictMode>
