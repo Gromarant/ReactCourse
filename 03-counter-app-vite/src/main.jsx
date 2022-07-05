57d5c3041263a36de4fd295209e5579c31f87e6d
Mariangelica Rodriguez <gromarant@gmail.com>
2022-07-05 16:03:46 +0200

    UseState and hooks, 45 lesson

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { EventApp } from './FirstApp';
+import { CounterAppHook } from './FirstApp';
 import './styles.css';
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        < EventApp value={ 100000 }/>
+        < CounterAppHook value={ 100 }/>
     </React.StrictMode>
 );
 
 
-// imports
+// All imports made
     // import { DefPropsApp } from './FirstApp';
     // import { PropsApp } from './FirstApp';
     // import { FirstApp } from './FirstApp';
     // import { HelloWordApp } from './HelloWordApp';
     // import { CounterApp } from './CounterApp';
+    // import { EventApp } from './FirstApp';
+    // import { CounterApp1000 } from './FirstApp';
 
 
-//Rendering of components
+//All components Rendering
     // <React.StrictMode>
     //     {/* <HelloWordApp /> */}
     //     {/* <FirstApp subtitle={25000} /> */}
     //     < PropsApp title="The salary of 50.000 Euros is for you" />
         // < DefPropsApp title="The salary of 50.000 Euros is for you" />
         // < CounterApp value={ 100000 }/>
+        // < EventApp value={ 100000 }/>
+        // < CounterApp1000 value={ 10 }/>
     // </React.StrictMode>
