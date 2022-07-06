af22a2cf1695f47cf0dca1e0e199363aa15ee344
Mariangelica Rodriguez <gromarant@gmail.com>
2022-07-06 11:49:26 +0200

    Section 5 init

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,34 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CounterAppHandle } from './FirstApp';
+import {  } from '';
 import './styles.css';
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        < CounterAppHandle value={ 0 }/>
+        < />
     </React.StrictMode>
 );
-
-
-// All imports made
-    // import { DefPropsApp } from './FirstApp';
-    // import { PropsApp } from './FirstApp';
-    // import { FirstApp } from './FirstApp';
-    // import { HelloWordApp } from './HelloWordApp';
-    // import { CounterApp } from './CounterApp';
-    // import { EventApp } from './FirstApp';
-    // import { CounterApp1000 } from './FirstApp';
-    // import { CounterAppHook } from './FirstApp';
-
-
-//All components Rendering
-    // <React.StrictMode>
-    //     {/* <HelloWordApp /> */}
-    //     {/* <FirstApp subtitle={25000} /> */}
-    //     < PropsApp title="The salary of 50.000 Euros is for you" />
-        // < DefPropsApp title="The salary of 50.000 Euros is for you" />
-        // < CounterApp value={ 100000 }/>
-        // < EventApp value={ 100000 }/>
-        // < CounterApp1000 value={ 10 }/>
-        // < CounterAppHook value={ 100 }/>
-        // </React.StrictMode>
