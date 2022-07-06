edfaf7bbae99ce3747ab5f84b2fa4d9aa9c3e605
Mariangelica Rodriguez <gromarant@gmail.com>
2022-07-06 11:30:57 +0200

    Refactor version of homework CounterAppHandle

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,21 +1,22 @@
 //lesson 45 handleSubtract and handleReset
+  //Refactor version
   import { useState } from 'react';
   import PropTypes from 'prop-types';
 
   export const CounterAppHandle = ( { value } ) => {
 
     const [ counter, setCounter ] = useState( value );    //useState set the initial value
-    const handleEvent = () => {
-      setCounter ( counter + 1 );   //Another way, without the value of 'counter'
-    };                    //setCounter( ( c ) => c + 1 );  c = current value of counter
+    const handleEvent = () => setCounter ( counter + 1 ); 
+    const handleSubtract = () => setCounter( counter -1 );
+    const handleReset = () => setCounter( value );              //setCounter( ( c ) => c + 1 );  c = current value of counter
 
     return (
       <>
         <h1>handleSubtract and handleReset</h1>
         <h2> { counter } </h2>
         <button onClick={ handleEvent }> +1 </button>
-        <button onClick={ () => setCounter( counter -1 ) }> -1 </button>
-        <button onClick={ () => setCounter( value ) }> Reset </button>
+        <button onClick={ handleSubtract }> -1 </button>
+        <button onClick={ handleReset }> Reset </button>
       </>
     )
   }
