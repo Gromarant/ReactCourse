00551cd2c79e6f0761a435d6e4a891cb94d49f8d
Mariangelica Rodriguez <gromarant@gmail.com>
2022-07-07 10:34:58 +0200

    Expect().toBe() jest, test helper download

--- 03-counter-app-vite/tests/demo.test.js
+++ 03-counter-app-vite/tests/demo.test.js
@@ -1,5 +1,11 @@
 test( 'this test must not fail', () => {
-    if( 1 === 0 ) {
-        throw new Error( 'you cannot divide by 0' );
-    }
+   
+    //1. Arrange
+        const message1 = "First test";
+
+    //2. Act
+        const message2 = message1.trim();
+
+    //3. Assert 
+        expect( message1 ).toBe( message2 );
 })
