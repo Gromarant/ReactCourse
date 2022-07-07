ce454d3b457982909c76273566b53563267d18dc
Mariangelica Rodriguez <gromarant@gmail.com>
2022-07-07 10:42:30 +0200

    description of the tested file

--- 03-counter-app-vite/tests/demo.test.js
+++ 03-counter-app-vite/tests/demo.test.js
@@ -1,11 +1,14 @@
-test( 'this test must not fail', () => {
+describe('Test in <DemoComponent />', () => {
+    
+    test( 'this test must not fail', () => {
    
-    //1. Arrange
-        const message1 = "First test";
-
-    //2. Act
-        const message2 = message1.trim();
-
-    //3. Assert 
-        expect( message1 ).toBe( message2 );
-})
+        //1. Arrange
+            const message1 = "First test";
+    
+        //2. Act
+            const message2 = message1.trim();
+    
+        //3. Assert 
+            expect( message1 ).toBe( message2 );
+    })
+ })
