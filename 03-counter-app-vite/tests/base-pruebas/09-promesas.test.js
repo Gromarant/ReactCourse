83b0a5ac3b4a6d70253cf22c5bfe388140179cc8
Mariangelica Rodriguez <gromarant@gmail.com>
2022-07-08 19:22:34 +0200

    09-promesas.js asynchronous test with "catch();" and "done();"

--- 03-counter-app-vite/tests/base-pruebas/09-promesas.test.js
+++ 03-counter-app-vite/tests/base-pruebas/09-promesas.test.js
@@ -4,7 +4,7 @@ describe('Test to 09-promesas.js', () => {
     
     test('1. getHeroeByIdAsync, must be return an hero', ( done ) => { 
         const id = 1;
-        
+
         getHeroeByIdAsync( id )
             .then( hero => {
                 expect( hero ).toEqual({
@@ -18,4 +18,14 @@ describe('Test to 09-promesas.js', () => {
 
      });
 
+     test('2. getHeroeByIdAsync, must be throw an Error if hero does not exist', ( done ) => { 
+        const id = 20;
+        
+        getHeroeByIdAsync( id )
+            .catch( error => {
+                expect( error ).toBe(`No se pudo encontrar el héroe con el id: ${ id }`);
+                console.log( error);
+                done();
+            });
+     });
  });
