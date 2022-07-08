113baa411be42050bc4716a5085870913ad20f7c
Mariangelica Rodriguez <gromarant@gmail.com>
2022-07-08 14:38:22 +0200

    06-deses-obj.js add individual tests

--- 03-counter-app-vite/tests/base-pruebas/06-deses-obj.test.js
+++ 03-counter-app-vite/tests/base-pruebas/06-deses-obj.test.js
@@ -1,12 +1,12 @@
 import { usContext } from "../../src/base-pruebas/06-deses-obj";
 
 describe('Test to 06-deses-obj.js', () => { 
-
+    
     test('usContext must be return an object', () => { 
-        
         const clave = 'chosto';
         const edad = 90;
         const result = usContext({ clave, edad });
+        
         
         expect( result.nombreClave ).toBe( clave );
         expect( result.anios ).toBe( edad );
@@ -15,4 +15,23 @@ describe('Test to 06-deses-obj.js', () => {
             lng: -12.3232
         });
      });
+
+    // Individual test, they are more specifics and easy to reviw if they failed
+        // Test clave
+            //  it('returns clave as nombreClave property', () => { 
+            //      expect( result.nombreClave ).toBe( clave );
+            //   });
+
+        // Test edad
+            //   test('usContext returns "edad" in "anios" property', () => {
+            //     expect( result.anios ).toBe( edad );
+            //   });
+
+        // Test latlng
+            //   test('fixed latlng property values', () => {
+            //     expect( result.latlng ).toEqual({
+            //         lat: 14.1232,
+            //         lng: -12.3232
+            //     });
+            //   })
  }); 
