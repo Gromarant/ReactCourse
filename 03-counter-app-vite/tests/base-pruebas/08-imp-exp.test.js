2907f56a469f99f9dc6106a53bc5a2253fcf6dff
Mariangelica Rodriguez <gromarant@gmail.com>
2022-07-08 18:52:00 +0200

    08-impo-exp.js Homework-getHeroesByOwner

--- 03-counter-app-vite/tests/base-pruebas/08-imp-exp.test.js
+++ 03-counter-app-vite/tests/base-pruebas/08-imp-exp.test.js
@@ -1,11 +1,12 @@
 import { getHeroeById, getHeroesByOwner } from "../../src/base-pruebas/08-imp-exp";
+import heroes from "../../src/data/heroes";
 
 describe('Test to 08-impo-exp.js', () => { 
     
-    test('getHeroeById must be return an heroe by Id', () => { 
-
+    test('1. getHeroeById must be return an heroe by Id', () => { 
         const id = 1;
         const heroe = getHeroeById( id );
+
         expect( heroe ).toEqual({
             id: 1,
             name: 'Batman',
@@ -14,12 +15,35 @@ describe('Test to 08-impo-exp.js', () => {
 
     });
 
-    test('getHeroeByI must be return undefined if the id does not exist', () => { 
-
+    test('2. getHeroeByI must be return undefined if the id does not exist', () => { 
         const id = 100;
         const heroe = getHeroeById( id );
+
         expect( heroe ).toBeFalsy();
 
     });
 
+//Homework 
+//1. must be return an array with the DC heroes (Length === 3)
+//2. use toEqual for evaluate each hero from the array
+//3. must be return an array with the Marvel heroes (length === 2)
+
+    test('3. return an array with the DC objects', () => {
+        const owner = 'DC';
+        const hero = getHeroesByOwner( owner );
+        
+        expect( hero.length ).toBe( 3 );
+        expect( hero ).toEqual( heroes.filter( (heroe) => heroe.owner === owner ));
+    })  
+    
+
+    test('4. return an array with the Marvel objects', () => {
+        const owner = 'Marvel';
+        const hero = getHeroesByOwner( owner );
+
+        expect( hero.length ).toBe( 2 );
+        expect( hero ).toEqual( heroes.filter( (heroe) => heroe.owner === owner ));
+    })
+
  });
+
