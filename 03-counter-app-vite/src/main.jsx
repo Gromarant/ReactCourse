25725236cc5653a0bc2a47595de67a2be3ce72fe
Mariangelica Rodriguez <gromarant@gmail.com>
2022-07-12 12:49:51 +0200

    Add FirstApp.jsx file, section 5, lesson 61

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CounterAppTest } from './Test';
+import { FirstApp } from './section4/FirstApp';
 import './styleGlobal.css';
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        < CounterAppTest value={ 0 }/>
+        < FirstApp />
     </React.StrictMode>
 );
---
+++ 03-counter-app-vite/src/section4/FirstApp.jsx
@@ -0,0 +1,16 @@
+import Proptypes from 'prop-types';
+
+export const FirstApp = ({ title, subTitle, name }) => {
+  return (
+    <>
+        <h1>{ title }</h1>
+        <h3>{ subTitle }</h3>
+        <p>{ name }</p>
+    </>
+  )
+}
+
+FirstApp.Proptypes = {
+    title: Proptypes.string.isRequired,
+    subTitle: Proptypes.string,
+}
