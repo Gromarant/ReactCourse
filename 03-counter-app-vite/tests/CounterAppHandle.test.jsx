8fd763021f9be0ddaf8b15399d65b05658f0a983
Mariangelica Rodriguez <gromarant@gmail.com>
2022-07-16 01:16:50 +0200

    Test to <CounterAppHandle /> with screen.getByText

--- 03-counter-app-vite/tests/CounterAppHandle.test.jsx
+++ 03-counter-app-vite/tests/CounterAppHandle.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { CounterAppHandle } from '../src/section4/CounterAppHandle';
 
 describe('Test to <CounterAppHandle />', () => {
@@ -17,4 +17,24 @@ describe('Test to <CounterAppHandle />', () => {
 
     });
 
+    test('3. should be increased plus one the counter with de "button +1"', () => { 
+
+        render( <CounterAppHandle value={ value } /> );
+        fireEvent.click( screen.getByText( '+1' ));
+        expect( screen.getByText( '21' ) ).toBeTruthy();
+     })
+
+     test('4. should be decreased menos one the counter with de "button -1"', () => { 
+
+        render( <CounterAppHandle value={ value } /> );
+        fireEvent.click( screen.getByText( '-1' ));
+        expect( screen.getByText( '19' ) ).toBeTruthy();
+     })
+
+     test('5. should be work the reset button', () => { 
+
+        render( <CounterAppHandle value={ value } /> );
+        fireEvent.click( screen.getByText( 'Reset' ));
+        expect( screen.getByText( value ) ).toBeTruthy();
+     })
 });
